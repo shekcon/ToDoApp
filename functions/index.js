b9b55329c6bbd5e440f8afd04bca444811f532a3
Shekcon <quangsang9773@gmail.com>
2020-04-17 21:24:41 +0700

    + Add CORS

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,32 @@
 const functions = require("firebase-functions");
 const app = require("express")();
+const cors = require("cors");
 
 const auth = require("./util/auth");
-
 const todoAPI = require("./apis/todos");
-
 const userAPI = require("./apis/users");
 
+app.use(
+  cors({
+    origin: ["https://todo-edb11.web.app", "http://localhost:3000"],
+    // origin: "*",
+    methods: "GET,PUT,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    maxAge: 86400,
+  })
+);
+
+// auth API
 app.post("/login", userAPI.loginUser);
 app.post("/signup", userAPI.signUpUser);
 
+// user API
 app.post("/user/image", auth, userAPI.uploadProfilePhoto);
 app.get("/user", auth, userAPI.getUserDetail);
 app.post("/user", auth, userAPI.updateUserDetails);
 
+// todo API
 app.get("/todos", auth, todoAPI.getAllTodos);
 app.post("/todos", auth, todoAPI.postOneTodo);
 app.delete("/todo/:todoID", auth, todoAPI.deleteTodo);
