1dfb29d55e8bd9d98ce10f1ba404817f3f8b09cc
Shekcon <quangsang9773@gmail.com>
2020-04-17 21:38:55 +0700

    + Add Frontend

---
+++ view/src/App.js
@@ -0,0 +1,35 @@
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#FF5722",
+      dark: "#d50000",
+      contrastText: "#fff",
+    },
+  },
+});
+
+function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+            <Route exact path="/" component={home} />
+          </Switch>
+        </div>
+      </Router>
+    </MuiThemeProvider>
+  );
+}
+export default App;
